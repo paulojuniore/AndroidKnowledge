342a7c6b06f585d7dd4fc446e1e7c441b7bfdd97
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-01-30 19:48:25 -0300

    Toasts added to ToggleButton

--- Some Components/app/src/main/java/com/paulojuniore/somecomponents/MainActivity.java
+++ Some Components/app/src/main/java/com/paulojuniore/somecomponents/MainActivity.java
@@ -2,6 +2,9 @@ package com.paulojuniore.somecomponents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onToggleButtonClicked(View view) {
+        boolean on = ((ToggleButton) view).isChecked();
+        if(on) {
+            Toast toast = Toast.makeText(this, "Your tea will be cold!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            Toast toast = Toast.makeText(this, "Your tea will be hot!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }

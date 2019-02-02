b384cee4aba31f2b4af4ca8c5b5a6749e49e0d1c
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-02 12:07:26 -0300

    Problema com o ArrayAdapter resolvido.

--- ComputacaoUFCG/app/src/main/java/com/paulojuniore/computacaoufcg/Grade.java
+++ ComputacaoUFCG/app/src/main/java/com/paulojuniore/computacaoufcg/Grade.java
@@ -5,10 +5,22 @@ public class Grade {
     private String subjectName;
     private int workLoad;
     public static final Grade[] subjects = {
-            new Grade("Fund de Matemática P/ C. da Computação", 60),
+            new Grade("Fund de Matemática P/ C. da Computação I", 60),
             new Grade("Introdução a Computação", 60),
             new Grade("Laboratório de Programação I", 60),
-            new Grade("Programação I", 60)
+            new Grade("Programação I", 60),
+
+            new Grade("Cálculo Diferencial e Integral I", 60),
+            new Grade("Fund de Matemática P/ C. da Computação II", 60),
+            new Grade("Laboratório de Programação II", 60),
+            new Grade("Programação II", 60),
+
+            new Grade("Algébra Linear I", 60),
+            new Grade("Cálculo Diferencial e Integral II", 60),
+            new Grade("Estrutura de Dados e Algoritmos", 60),
+            new Grade("Laboratório de Estrutura de Dados e Algoritmos", 60),
+            new Grade("Lógica para Computação", 60),
+            new Grade("Teoria dos Grafos", 60)
     };
 
     private Grade(String subjectName, int workLoad) {

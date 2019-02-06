94cc821d5b3adff354cb6d745fd5c2ccc0663596
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-06 14:46:34 -0300

    Obtendo a referência do Fragmento e atualizando a View com bases nos dados da Classe Workout

--- Workout/app/src/main/java/com/paulojuniore/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/paulojuniore/workout/WorkoutDetailFragment.java
@@ -1,21 +1,43 @@
 package com.paulojuniore.workout;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+//import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workoutId;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkoutId(long workoutId) {
+        this.workoutId = workoutId;
     }
 
 }

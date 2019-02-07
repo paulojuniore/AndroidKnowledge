f008b62a6814960bafd46d6580b482b415fd50dc
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-07 14:57:49 -0300

    Salvando o estado da variavel workoutId no método onSaveInstanceState, para que quando o dispositivo for girado, o fragmento continue como antes.

--- Workout/app/src/main/java/com/paulojuniore/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/paulojuniore/workout/WorkoutDetailFragment.java
@@ -18,6 +18,9 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
@@ -39,4 +42,8 @@ public class WorkoutDetailFragment extends Fragment {
         this.workoutId = workoutId;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
+    }
 }

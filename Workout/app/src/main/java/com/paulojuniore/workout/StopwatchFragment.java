279f03cb58bb5d6057b162f977b9ce5a6b287fad
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-08 21:01:00 -0300

    Adaptando o Fragmento StopWatch, métodos de ciclo de vida públicos e setando o layout de fragmento.

--- Workout/app/src/main/java/com/paulojuniore/workout/StopwatchFragment.java
+++ Workout/app/src/main/java/com/paulojuniore/workout/StopwatchFragment.java
@@ -2,10 +2,12 @@ package com.paulojuniore.workout;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +15,82 @@ import android.view.ViewGroup;
  */
 public class StopwatchFragment extends Fragment {
 
-
-    public StopwatchFragment() {
-        // Required empty public constructor
-    }
-
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        return layout;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(wasRunning){
+            running = true;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    // Inicia o cronômetro quando o botão Start é clicado.
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    // Para o cronômetro quando o botão Stop é clicado.
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    // Reinicia o cronômetro quando o botão Reset é clicado.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(View view) {
+        final TextView timeView = (TextView) view.findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
 }

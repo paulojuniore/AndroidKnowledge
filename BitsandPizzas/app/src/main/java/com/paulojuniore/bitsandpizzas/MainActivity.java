0b0e58e46f8f7463ce526fc4275a4e5eaddb7b77
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-11 13:39:12 -0300

    Inflando o menu na atividade com o método onCreateOptionsMenu().

--- BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
@@ -2,6 +2,7 @@ package com.paulojuniore.bitsandpizzas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflates the menu; this adds items to the action bar if present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

1c9594ad251efd97cc70715422be36b4f15ebb44
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-13 14:37:16 -0300

    Adicionando guia para criar um novo pedido e criando esta atividade e iniciando a intenção em onOptionsItemSelected(Menu menu).

--- BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
@@ -1,8 +1,10 @@
 package com.paulojuniore.bitsandpizzas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -18,4 +20,19 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }

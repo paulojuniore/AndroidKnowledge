4410643abb4d885ab5bf7c09a278f85b4af1b986
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-13 15:40:02 -0300

    Atualizando...

--- DesignLibrary/app/src/main/java/com/paulojuniore/designlibrary/MainActivity.java
+++ DesignLibrary/app/src/main/java/com/paulojuniore/designlibrary/MainActivity.java
@@ -1,13 +1,29 @@
 package com.paulojuniore.designlibrary;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Trate os eventos de navegação aqui
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }

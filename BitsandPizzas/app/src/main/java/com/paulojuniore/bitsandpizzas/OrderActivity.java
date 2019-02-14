3f9dac65d463c8b03573f19c8d6f72eec98f06fe
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-13 22:18:25 -0300

    Configurando a atividade pai de OrderActivity e adicionando um botão Up em OrderActivity para voltar a MainActivity.

--- BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/OrderActivity.java
+++ BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/OrderActivity.java
@@ -2,6 +2,7 @@ package com.paulojuniore.bitsandpizzas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.ActionBar;
 
 public class OrderActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class OrderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }

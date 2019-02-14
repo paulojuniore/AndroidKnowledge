8ed4fd2654da406a0798f2ce7a7f1f118cad1edd
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-14 14:22:43 -0300

    Fazendo o ListView responder aos cliques usando um receptor OnItemClickListener, implementado o método onItemClick, e assim selecionando a partir da opção qual o fragmento deve ser iniciado.

--- BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/paulojuniore/bitsandpizzas/MainActivity.java
@@ -1,14 +1,42 @@
 package com.paulojuniore.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch (position) {
+            case 0:
+                fragment = new TopFragment();
+                break;
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+        }
+    }
 
     private String[] titles;
     private ListView drawerList;
@@ -21,6 +49,8 @@ public class MainActivity extends Activity {
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView) findViewById(R.id.drawer);
         drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles));
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     @Override

3d227e1d054454810c4d33b3bce4d2369ce5215f
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 16:18:32 -0300

    Adicionando um ActionBarDrawerToggle para criar um botão para abrir e fechar a caixa de navegação, além de deslizar.

--- BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
@@ -1,15 +1,28 @@
 package com.paulojuniore.bandp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
     }
 
 }

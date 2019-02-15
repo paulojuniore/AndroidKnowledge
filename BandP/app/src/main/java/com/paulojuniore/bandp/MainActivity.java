2ae3e7dd1666b170d8c49ba7a91468e1024b531f
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 15:54:40 -0300

    Cleaning...

--- BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
@@ -1,96 +1,15 @@
 package com.paulojuniore.bandp;
 
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String[] titles;
-    private ListView drawerList;
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            // Código a ser executado quando um item da caixa de navegação for clicado.
-            selectItem(position);
-        }
-    }
-
-    private void selectItem(int position) {
-        Fragment fragment = null;
-        switch (position) {
-            case 0:
-                fragment = new TopFragment();
-                break;
-            case 1:
-                fragment = new PizzaFragment();
-                break;
-            case 2:
-                fragment = new PastaFragment();
-                break;
-            case 3:
-                fragment = new StoreFragment();
-                break;
-        }
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.content_frame, fragment);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
-        // Fecha a caixa de navegação depois de um item ser clicado.
-        // drawerLayout.closeDrawer(drawerList);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        titles = getResources().getStringArray(R.array.titles);
-        drawerList = (ListView) findViewById(R.id.drawer);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                titles));
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-        if(savedInstanceState == null) {
-            selectItem(0);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_create_order:
-                Intent intent = new Intent(MainActivity.this, CreateOrderActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.action_share:
-                return true;
-            case R.id.action_settings:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        
     }
 
 }

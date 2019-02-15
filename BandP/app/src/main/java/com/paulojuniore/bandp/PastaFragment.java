216577d7f22157e15f405b6ccb40fa95fd88271c
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 00:52:10 -0300

    Adicionando arrays para as categorias do app e usando ArrayAdapter para carregá-los em seus respectivos ListFragment.

--- BandP/app/src/main/java/com/paulojuniore/bandp/PastaFragment.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/PastaFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * A simple {@link ListFragment} subclass.
@@ -15,6 +16,8 @@ public class PastaFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pastas));
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 

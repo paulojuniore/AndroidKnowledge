1175055ea4f11c0a31fa3dc101ca3a3920f11a0c
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 00:41:47 -0300

    Adicionando 4 fragmentos para gerenciarem as páginas do app.

---
+++ BandP/app/src/main/java/com/paulojuniore/bandp/PizzaFragment.java
@@ -0,0 +1,21 @@
+package com.paulojuniore.bandp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class PizzaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}

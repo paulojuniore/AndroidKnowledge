2011bfeb3a23cd21b43572e595e5ff01e04e458c
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 00:28:31 -0300

    onOptionsItemSelected implementado para tratar cliques em itens de ação.

---
+++ BandP/app/src/main/java/com/paulojuniore/bandp/CreateOrderActivity.java
@@ -0,0 +1,13 @@
+package com.paulojuniore.bandp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+}
--- BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.paulojuniore.bandp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +20,19 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(MainActivity.this, CreateOrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_share:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }

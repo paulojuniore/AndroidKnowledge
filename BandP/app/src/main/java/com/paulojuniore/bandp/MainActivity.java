caac2e4aca41761872655f440f3f846983eae072
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-15 01:12:37 -0300

    Obtendo referências para os itens de layout de activity_main e setando o adaptador do listView com o array de titles de strings.xml.

--- BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/MainActivity.java
@@ -1,17 +1,31 @@
 package com.paulojuniore.bandp;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1,
+                titles));
     }
 
     @Override

e3df525ea94d1165ba2980c79f31d799292ce12d
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-18 20:56:39 -0300

    Iniciando com banco de dados SQLite para gerenciar todos os dados do app Bits and Pizzas.

---
+++ BandP/app/src/main/java/com/paulojuniore/bandp/BitsandPizzasDataBaseHelper.java
@@ -0,0 +1,30 @@
+package com.paulojuniore.bandp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class BitsandPizzasDataBaseHelper extends SQLiteOpenHelper {
+
+    // Nome do banco de dados
+    private static final String DB_NAME = "bitsandpizzas";
+
+    // Versão do banco de dados
+    private static final int DB_VERSION = 1;
+
+    public BitsandPizzasDataBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}

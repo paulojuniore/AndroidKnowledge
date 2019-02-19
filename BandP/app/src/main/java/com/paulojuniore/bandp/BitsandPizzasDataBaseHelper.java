35d67b9771421e11f55971849e1aec915cf31755
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-19 13:57:05 -0300

    Criando um contentValues para armazenar os dados das respectivas coluna da tabela, e inserindo um novo registro na tabela com esses dados. Além de atualizar a tabela adicionando mais uma coluna para versões onde o banco de dados tem versão menor que 2.

--- BandP/app/src/main/java/com/paulojuniore/bandp/BitsandPizzasDataBaseHelper.java
+++ BandP/app/src/main/java/com/paulojuniore/bandp/BitsandPizzasDataBaseHelper.java
@@ -1,5 +1,6 @@
 package com.paulojuniore.bandp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,7 +12,7 @@ public class BitsandPizzasDataBaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "bitsandpizzas";
 
     // Versão do banco de dados
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     public BitsandPizzasDataBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -19,15 +20,35 @@ public class BitsandPizzasDataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE PIZZAS ("
-                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "NAME TEXT, "
-                + "DESCRIPTION TEXT);");
+        updateMyDataBase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDataBase(db, oldVersion, newVersion);
+    }
 
+    private void updateMyDataBase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE PIZZAS ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT);");
+
+            insertPizza(db, "Diavolo", "Delicious");
+            insertPizza(db, "Funghi", "Especial");
+            insertPizza(db, "Magheritta", "Very good");
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE PIZZAS ADD COLUMN FAVORITE NUMERIC");
+        }
+    }
+
+    private void insertPizza(SQLiteDatabase db, String name, String description) {
+        ContentValues pizzaValues = new ContentValues();
+        pizzaValues.put("NAME", name);
+        pizzaValues.put("DESCRIPTION", description);
+        db.insert("PIZZAS", null, pizzaValues);
     }
 
 }
